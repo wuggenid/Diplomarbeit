e3c29d58f367ec9c7390b201488a21660c505d70
wuggenid <danielwugg60@gmail.com>
2015-01-02 18:25:30 +0100

    test

--- pizza/app/database/seeds/xposSeeder.php
+++ pizza/app/database/seeds/xposSeeder.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Daniel
- * Date: 02.01.2015
- * Time: 17:32
- */ 
+
+class BestellungenSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0;$i<5;$i++)
+        {
+            $xpos = new xpos();
+            $xpos->RNR = -1;
+            $xpos->PNR = -1;
+            $xpos->GM = -1;
+            $xpos->ANR = -1;
+            $xpos->A0 = "Testbezeichnung";
+            $xpos->CB = -1;
+            $xpos->SU = -1;
+            $xpos->RNR = -1;
+            $xpos->save();
+        }
+    }
+
+}
