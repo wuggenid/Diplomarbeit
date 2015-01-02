26f21e3994d6786cf31f139a3b6ed88ae5551bb2
wuggenid <danielwugg60@gmail.com>
2015-01-02 18:20:58 +0100

    xpos Seeder & models umbenannt

--- pizza/app/models/Bestellung.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Bestellung extends Eloquent
-{
-    protected $table = 'xadress';
-    public $timestamps = false;
-}
---
+++ pizza/app/models/xadress.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 02.01.2015
+ * Time: 17:33
+ */ 
