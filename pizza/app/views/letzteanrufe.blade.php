7bb63e0783792abb843a6ed4cc124abdd6ab67c7
wuggenid <daniel.wuggenig@edu.htl-villach.at>
2015-02-11 11:44:12 +0100

    Letzte Anrufe v1.0

--- pizza/app/views/letzteanrufe.blade.php
+++ pizza/app/views/letzteanrufe.blade.php
@@ -4,6 +4,8 @@
 
 <h2>Letzte Anrufe</h2>
 
+<a href="/Bestellungen"> <button style="width: 7em;" class="btn btn-lg btn-danger"><span class="glyphicon glyphicon-chevron-left"></span> Zurück</button></a>
+
 <table class="table">
     <thead>
         <tr>
@@ -13,12 +15,13 @@
     </thead>
     <tbody>
         <?php
-            $anrufe = DB::table('xstamm')->take(15)->orderBy('RZT','desc')->get();
+            $anrufe = DB::select(DB::raw("SELECT * FROM `xstamm` ORDER BY unix_timestamp(date(RDT)) DESC, time(RZT) DESC LIMIT 15"));
             $kunden = array();
             for($i = 0;$i<15;$i++)
             {
                 $kunden[$i] = DB::table('xadress')->where('TEL','=',$anrufe[$i]->TEL)->get();
             }
+            $datetime = explode(' ',$anrufe[0]->RDT)[0].' '.explode(' ',$anrufe[0]->RZT)[1];
         ?>
         @for ($i = 0;$i<15;$i++)
             <tr>
@@ -28,5 +31,5 @@
         @endfor
     </tbody>
 </table>
-
+<a href="/Bestellungen"> <button style="width: 7em;" class="btn btn-lg btn-danger"><span class="glyphicon glyphicon-chevron-left"></span> Zurück</button></a>
 @stop
